0ae68786aaf392aa197eebf52c8170a125951a6c
Ignacio R Espino <irespino@capozios.com>
2018-07-18 03:57:49 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,11 @@
 def turn_count(arg_board)
   counter = 0
-  arg_board.each
+  arg_board.each {|space|
+  if space == "X" || space "O"
+    counter += 1
+  end
+  }
+  return counter
 end
 
 def current_player()
